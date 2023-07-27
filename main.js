1fd9dd8531367718ea4ac1dd35d2fe048e99b549
Thalita <119790076+FernandesThalita@users.noreply.github.com>
2023-07-26 21:40:55 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed("#text",{
-    strings:["Dev FullStack />","Front e Back />"],
+    strings:["Front e Back />","Dev FullStack />"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
