7fe0f14923f42b7e630380e87c84a44f704d0e2f
Thalita <119790076+FernandesThalita@users.noreply.github.com>
2023-07-28 16:45:10 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,24 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.titulo',{ 
+  duration: 1000,
+  rotate: {x:0,y:80,z:0}
+});
+sr.reveal('.linguagem',{ 
+  duration: 1000,
+  rotate: {x:0,y:20,z:100}
+});
+
+
+sr.reveal('header',{
+    duration:1000
+})
+
+
+sr.reveal('.card',{
+    duration:1200,
+    distance: '90px' 
+})
+sr.reveal('footer',{
+    duration:1500,
+})
