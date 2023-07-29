37be2c25426b3cc95dccfcfff64095d0c91371e3
Thalita <119790076+FernandesThalita@users.noreply.github.com>
2023-07-29 12:04:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-var typed = new Typed("#text",{
-    strings:["Front e Back />","Dev FullStack />"],
+var typed = new Typed("#taping",{
+    strings:["Front e Back;","FullStack;"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:450,
 });
+
+
+
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-menu .icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
--- reveal.js
+++ reveal.js
@@ -8,11 +8,36 @@ sr.reveal('.linguagem',{
   duration: 1000,
   rotate: {x:0,y:20,z:100}
 });
+sr.reveal('.logo',{ 
+  duration: 1000,
+  distance:'-50px'
+});
+sr.reveal('.nav-links',{ 
+  duration: 1000,
+  distance:'-50px'
+});
+sr.reveal('#hamburger',{ 
+  duration: 1000,
+  distance:'-50px'
+});
+sr.reveal('.p2 , .p1',{ 
+  duration: 1000,
+  rotate: {x:20,y:0,z:20},
+  easing: 'ease-in-out'
+
+});
+sr.reveal('.btn',{ 
+  duration: 1000,
+  easing: 'ease'
+
+});
+sr.reveal(' a',{ 
+  duration: 1000,
+  easing: 'ease'
+});
 
 
-sr.reveal('header',{
-    duration:1000
-})
+
 
 
 sr.reveal('.card',{
